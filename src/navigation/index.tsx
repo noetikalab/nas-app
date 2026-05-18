d4dcd8dfedbaeb59f42a246fccb6b9d8da8260e9
张利 <2475934003@qq.com>
2026-05-18 20:10:54 +0800

    feat(navigation): setup stack navigator with Login/Home/DevSettings routes

---
+++ src/navigation/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {LoginScreen} from '../screens/LoginScreen';
+import {HomeScreen} from '../screens/HomeScreen';
+import {DevSettingsScreen} from '../screens/DevSettingsScreen';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Home: undefined;
+  DevSettings: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="DevSettings" component={DevSettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
