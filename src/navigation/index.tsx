5bf372e2e89e9dc168c97f055093614dc7d61787
张利 <2475934003@qq.com>
2026-05-19 14:41:18 +0800

    chore(navigation): set initial route to Home for development

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -16,7 +16,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="DevSettings" component={DevSettingsScreen} />
